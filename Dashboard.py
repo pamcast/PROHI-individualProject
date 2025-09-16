473cd2f1a5127e08fcc4a5abbb5ae10ee3a8fcb5
pamcast <pamela.cast.glez@gmail.com>
2025-09-17 00:37:06 +0200

    Added a forms
    Added a forms with some of the features and the submit button that does not do an action

--- Dashboard.py
+++ Dashboard.py
@@ -57,8 +57,10 @@ if page == "Home":
     with st.form("my_form"):
         sex = st.text_input("Enter your gender")
         age = st.number_input("Enter your age", min_value=1)
-        blood_pressure: st.number_input("Enter systolic blood pressure")
-    
+        blood_pressure: st.number_input("Enter systolic blood pressure") # type: ignore
+        cholesterol= st.number_input("Enter cholesterol in mmol/L")
+        blood_sugar = st.number_input("Add fasting blood sugar")
+        
         submitted = st.form_submit_button("Submit")
 
 
