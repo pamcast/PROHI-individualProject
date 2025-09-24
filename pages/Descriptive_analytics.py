889a244060850ce2210dbd32d7f4822e6624adcc
pamcast <pamela.cast.glez@gmail.com>
2025-09-24 20:31:46 +0200

    changes

--- pages/Descriptive_analytics.py
+++ pages/Descriptive_analytics.py
@@ -1,32 +1,11 @@
-import numpy as np
-import pandas as pd
+import streamlit as st
 
-import matplotlib.pyplot as plt
-import seaborn as sns
-import plotly.express as px
+def load_data():
+    import pandas as pd
+    df = pd.read_csv('healthcare-dataset-stroke-data.csv')
+    return df
 
-import sklearn
-import shap
-
-df = pd.read_csv("healthcare-dataset-stroke-data.csv")
-df.to_csv("healthcare-dataset-stroke-data.csv", index = False)
-df.head()
-
-df = df.rename(columns={
-                          "id":"ID",
-                          "gender": "Sex",
-                          "age": "Age",
-                          "hypertension": "Hypertension",
-                          "heart_disease": "Heart Disease",
-                          "ever_married": "Ever Married?",
-                          "work_type": "Work Type",
-                          "Residence_type": "Residence Type",
-                          "avg_glucose_level": "Avg Glucose Level",
-                          "bmi": "BMI",
-                          "smoking_status": "Smoking?",
-                          "stroke": "Ever had a Stroke?"
-                          })
-df.head(10)
+df = load_data()
 
 # Percentage of women
 percentage_women = (df['Sex'] =='Female').mean() * 100
@@ -37,7 +16,7 @@ percentage_men = (df['Sex'] =='Male').mean() * 100
 #Mean age 
 mean_age = df["Age"].mean()
 
-col1, col2 = st.columns(3)
+col1, col2, col3 = st.columns(3)
 col1.metric("Percentage of women (%)", f"{percentage_women: .2f}", border=True)
 col2.metric("Percentage of men (%)", f"{percentage_men: .2f}", border=True)
 col2.metric("Percentage of men (%)", f"{mean_age: .2f}", border=True)
