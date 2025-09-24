e47d1a842c18dfc7e9d892e5fb2fdb5589aaa1d1
pamcast <pamela.cast.glez@gmail.com>
2025-09-24 20:13:34 +0200

    changes

--- pages/Descriptive_analytics.py
+++ pages/Descriptive_analytics.py
@@ -1,3 +1,33 @@
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+
+import sklearn
+import shap
+
+df = pd.read_csv("healthcare-dataset-stroke-data.csv")
+df.to_csv("healthcare-dataset-stroke-data.csv", index = False)
+df.head()
+
+df = df.rename(columns={
+                          "id":"ID",
+                          "gender": "Sex",
+                          "age": "Age",
+                          "hypertension": "Hypertension",
+                          "heart_disease": "Heart Disease",
+                          "ever_married": "Ever Married?",
+                          "work_type": "Work Type",
+                          "Residence_type": "Residence Type",
+                          "avg_glucose_level": "Avg Glucose Level",
+                          "bmi": "BMI",
+                          "smoking_status": "Smoking?",
+                          "stroke": "Ever had a Stroke?"
+                          })
+df.head(10)
+
 # Percentage of women
 percentage_women = (df['Sex'] =='Female').mean() * 100
 
